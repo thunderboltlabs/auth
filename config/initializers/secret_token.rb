60ed016eddc64ce41f930492b04d2eb05f60e567
Tammer Saleh <tsaleh@gmail.com>
2013-03-16 10:17:19 -0700

    rails new auth -d postgresql -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Auth::Application.config.secret_key_base = '9e67b32185041e268b2fac99c12513223dd1ed023d6de5a8e0a1708f40ff99ea56300a5106f0a514fc30b2bf8bb873b99cbbaf17dfa168cedafebf5f82ff548d'

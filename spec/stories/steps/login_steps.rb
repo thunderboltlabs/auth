f07ecc27cf45cba15ef0084fe3b3e1b2a2a6b3d9
Tammer Saleh <tsaleh@gmail.com>
2013-03-16 11:52:55 -0700

    Added User model

--- spec/stories/steps/login_steps.rb
+++ spec/stories/steps/login_steps.rb
@@ -1,9 +1,13 @@
 step ":name is a user" do |name|
-  FactoryGirl.create(:user, name: name)
+  FactoryGirl.create(:user, name: name, password: "sekrit")
 end
 
 step ":name logs in" do |name|
-
+  user = User.find_by_name(name)
+  visit "/"
+  fill_in "email", with: user.email
+  fill_in "password", with: "sekrit"
+  click "Login"
 end
 
 step ":name is logged in" do |name|

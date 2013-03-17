018b670ae786903efb9cef905bd15f8d9a960575
Tammer Saleh <tsaleh@gmail.com>
2013-03-17 09:44:44 -0700

    Added users controller for root

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Auth::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root to: 'welcome#index'
+  root to: 'users#me'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
